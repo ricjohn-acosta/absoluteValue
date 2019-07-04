84390bbbafb8e2389988e30707263c803c1c9b27
ricjohn-acosta <ricjohn.genoguin@gmail.com>
2019-07-05 04:07:07 +1200

    created fibonacci algorithm + added comments

---
+++ fibonacci.js
@@ -0,0 +1,12 @@
+const fib = (x) => {
+    let array = [0, 1];
+
+    // Fibonacci algorithm.
+    for (let i = 2; i < x + 1; i++) {
+        array.push(array[i - 2] + array[i - 1])
+    }
+    // Remove first element and final element.
+    array.shift();
+    array.pop();
+    return array
+}
